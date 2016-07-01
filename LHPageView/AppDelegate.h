a4c02ce741e18396b315fe70924a278ffe150b42
tdywtz <654475310@qq.com>
2016-07-01 16:31:15 +0800

    Initial Commit

---
+++ LHPageView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LHPageView
+//
+//  Created by bangong on 16/7/1.
+//  Copyright © 2016年 auto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LHPageView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LHPageView
+//
+//  Created by bangong on 16/7/1.
+//  Copyright © 2016年 auto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
